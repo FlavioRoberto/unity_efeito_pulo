ca0570527de43fe5b6ffb3a226a210f804a4f394
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-05-16 19:46:33 -0300

    Add colect iten action

---
+++ Assets/Scripts/GetItem.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GetItem : MonoBehaviour
+    {
+        public string TagObject;
+        public GameObject ActionColider;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag(TagObject))
+            {
+                ActionColider.GetComponent<IActionColider>().Colision();
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pontuation.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Pontuation : MonoBehaviour, IActionColider
+    {
+        public int Score;
+
+        public Pontuation()
+        {
+            Score = 0;
+        }
+
+        public void Colision()
+        {
+            Score += 10;
+        }
+    }
+}
