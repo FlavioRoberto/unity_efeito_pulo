20a8fbec061b6086a0c2af1253500ed26b894c09
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-05-16 15:28:33 -0300

    Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    public float Velocidade = 10;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidBody.velocity = Vector3.right * Velocidade * Time.deltaTime;
+        }
+    }
+}
